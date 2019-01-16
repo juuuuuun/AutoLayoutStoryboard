b3a0f57ff30eaa1f79891de2b1b6dab17a2b474a
Jun Oh <kyungjunjoh@gmail.com>
2019-01-15 17:22:45 -0800

    Initial Commit

---
+++ AutoLayoutStoryboard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoLayoutStoryboard
+//
+//  Created by Jun Oh on 2019-01-15.
+//  Copyright © 2019 Jun Oh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AutoLayoutStoryboard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoLayoutStoryboard
+//
+//  Created by Jun Oh on 2019-01-15.
+//  Copyright © 2019 Jun Oh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
